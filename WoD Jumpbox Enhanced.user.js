f9179a76cd4a213a63e93d7c566061db18da5bc6
DotIN13 <34450555+DotIN13@users.noreply.github.com>
2020-02-12 10:38:36 +0800

    Update to 0.4
    workaround for GM_addStyle

---
+++ WoD Jumpbox Enhanced.user.js
@@ -0,0 +1,63 @@
+// ==UserScript==
+// @name         WoD Jumpbox Enhanced
+// @namespace    github.com/DotIN13
+// @version      0.4
+// @description  Easier jumpbox search
+// @author       DotIN13
+// @updateURL    https://github.com/DotIN13/WoD_Jumpbox_Enhanced/raw/master/Jumpbox%20Enhanced.user.js
+// @match        http://canto.world-of-dungeons.org/*
+// @grant        none
+// ==/UserScript==
+
+(function()
+{
+    'use strict';
+
+    //get jumpboxSpan
+    var jumpboxSpan = document.getElementById("jumpbox_center");
+
+    //create jumpboxSelect
+    var jumpboxSelect = document.createElement("select");
+    jumpboxSelect.id = "jumpboxSelect";
+    jumpboxSelect.style = "width: 50px;margin-right: 20px;height: 18px;"
+    jumpboxSpan.appendChild(jumpboxSelect);
+
+    //add select options
+    var jumpboxSelObj = document.getElementById("jumpboxSelect");
+    var addItemList = function()
+    {
+        var itemList = ["item", "set", "hero", "player", "skill", "npc", "post", "group", "clan", "auction", "class"],
+            itemChn = ["物品", "套装", "角色", "玩家", "技能", "NPC", "帖子", "团队", "联盟", "拍卖", "职业"];
+        for (var i = 0; i < itemList.length; i++)
+        {
+            var option = document.createElement("option");
+            option.value = (itemList[i]);
+            option.text = (itemChn[i]);
+            jumpboxSelObj.add(option);
+        }
+    }
+    addItemList();
+
+    //reroute jumpbox Button
+    var jumpboxBtn = document.querySelectorAll("#jumpbox_center>form>span>input");
+    var jumpbox = document.querySelector('#jumpbox_center>form>input[name="link"]');
+    jumpboxBtn[0].setAttribute("onclick", "window.jumper();return false;");
+
+    window.jumper = function()
+    {
+        var jumpbox = document.querySelector('#jumpbox_center>form>input[name="link"]');
+        var jumpboxValue = jumpbox.value;
+        var regtest = /^\s*\[\s*([^:]+?)\s*:\s*(.+?)\s*\]\s*$/;
+        var indicator = regtest.test(jumpboxValue);
+        if (indicator)
+        {
+            wodlink(jumpboxValue);
+        }
+        else
+        {
+            var jumplink = "[" + jumpboxSelObj.value + ":" + jumpboxValue + "]";
+            wodlink(jumplink);
+        }
+        jumpbox.value = "";
+    }
+})();
