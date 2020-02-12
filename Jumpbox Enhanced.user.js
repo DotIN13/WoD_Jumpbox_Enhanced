ebe49d394a2ec5dbf8a698093f3adc08db77f275
DotIN13 <34450555+DotIN13@users.noreply.github.com>
2020-02-12 10:44:20 +0800

    update to 0.4
    workaround for GM_addStyle

--- Jumpbox Enhanced.user.js
+++ Jumpbox Enhanced.user.js
@@ -1,24 +1,17 @@
 // ==UserScript==
 // @name         WoD Jumpbox Enhanced
 // @namespace    github.com/DotIN13
-// @version      0.3
+// @version      0.4
 // @description  Easier jumpbox search
 // @author       DotIN13
 // @updateURL    https://github.com/DotIN13/WoD_Jumpbox_Enhanced/raw/master/Jumpbox%20Enhanced.user.js
 // @match        http://canto.world-of-dungeons.org/*
-// @grant        GM_addStyle
+// @grant        none
 // ==/UserScript==
 
 (function()
 {
     'use strict';
-
-    //styling jumpboxSelect
-    GM_addStyle("#jumpboxSelect{width: 50px;margin-right: 20px;height: 18px;}");
-
-    //modifying tooltip
-    var jumpboxTooltip = document.querySelector("#jumpbox_center>form>span");
-    jumpboxTooltip.setAttribute("onmouseover","return wodToolTip(this,'<p>键入物品、英雄等名称，<br />选择相应类别，<br />按下按钮获知详情。<br />同时兼容[*:*]类型代码。<br /><br /><i>JumpBox Enhanced by DotIN13</i></p>');")
 
     //get jumpboxSpan
     var jumpboxSpan = document.getElementById("jumpbox_center");
@@ -26,23 +19,24 @@
     //create jumpboxSelect
     var jumpboxSelect = document.createElement("select");
     jumpboxSelect.id = "jumpboxSelect";
+    jumpboxSelect.style = "width: 50px;margin-right: 20px;height: 18px;"
     jumpboxSpan.appendChild(jumpboxSelect);
 
     //add select options
-    var itemList = ["item", "set", "hero", "player", "skill", "npc", "post", "group", "clan", "auction", "class"],
-        itemChn = ["物品", "套装", "角色", "玩家", "技能", "NPC", "帖子", "团队", "联盟", "拍卖", "职业"];
     var jumpboxSelObj = document.getElementById("jumpboxSelect");
-    var addItemList = function(list)
+    var addItemList = function()
     {
+        var itemList = ["item", "set", "hero", "player", "skill", "npc", "post", "group", "clan", "auction", "class"],
+            itemChn = ["物品", "套装", "角色", "玩家", "技能", "NPC", "帖子", "团队", "联盟", "拍卖", "职业"];
         for (var i = 0; i < itemList.length; i++)
         {
             var option = document.createElement("option");
             option.value = (itemList[i]);
-            option.text = (itemChn[i])
+            option.text = (itemChn[i]);
             jumpboxSelObj.add(option);
         }
     }
-    addItemList(itemList);
+    addItemList();
 
     //reroute jumpbox Button
     var jumpboxBtn = document.querySelectorAll("#jumpbox_center>form>span>input");
